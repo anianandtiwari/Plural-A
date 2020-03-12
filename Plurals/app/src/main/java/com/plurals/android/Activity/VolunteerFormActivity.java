39440497cb088286c1af1ba2797dbcfa682cde55
Ashish Anand <ashishanand@groomefy.com>
2020-03-12 11:48:11 +0530

    form page design & pc ac logic

--- Plurals/app/src/main/java/com/plurals/android/Activity/VolunteerFormActivity.java
+++ Plurals/app/src/main/java/com/plurals/android/Activity/VolunteerFormActivity.java
@@ -1,8 +1,10 @@
 package com.plurals.android.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.res.AssetManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
